366a0480a580b5b6575f24dde8cd9295102deaa3
Eduardo Chacana <168044501+Eslayer@users.noreply.github.com>
2024-11-26 09:21:18 -0300

    agregue todo lo necesario a Empleado.java

--- src/Modelo/Empleado.java
+++ src/Modelo/Empleado.java
@@ -10,4 +10,82 @@ package Modelo;
  */
 public class Empleado {
     
+    private int idEmpleado;
+    private int rut;
+    private String nombreEmpleado,appaterno,apmaterno;
+    private int sueldo;
+
+
+    public Empleado() {
+    }
+
+    public Empleado(int idEmpleado, int rut, String nombreEmpleado, String appaterno, String apmaterno, int sueldo) {
+        this.idEmpleado = idEmpleado;
+        this.rut = rut;
+        this.nombreEmpleado = nombreEmpleado;
+        this.appaterno = appaterno;
+        this.apmaterno = apmaterno;
+        this.sueldo = sueldo;
+    }
+
+    
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getAppaterno() {
+        return appaterno;
+    }
+
+    public void setAppaterno(String appaterno) {
+        this.appaterno = appaterno;
+    }
+
+    public String getApmaterno() {
+        return apmaterno;
+    }
+
+    public void setApmaterno(String apmaterno) {
+        this.apmaterno = apmaterno;
+    }
+
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "idEmpleado=" + idEmpleado + ", rut=" + rut + ", nombreEmpleado=" + nombreEmpleado + ", appaterno=" + appaterno + ", apmaterno=" + apmaterno + ", fechanac=" + ", sueldo=" + sueldo + '}';
+    }
+
+   
+    
 }
