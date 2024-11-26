1ab883cc7e6541237ba58f21f311c59db52efb8e
Eduardo Chacana <168044501+Eslayer@users.noreply.github.com>
2024-11-26 09:22:39 -0300

    agregue todo lo necesario a Departamento.java
    atributos, constructors, accesadores, getters, todo.

--- src/Modelo/Departamento.java
+++ src/Modelo/Departamento.java
@@ -10,4 +10,39 @@ package Modelo;
  */
 public class Departamento {
     
+    private int idDept;
+    private String nombreDept;
+
+    public Departamento() {
+    }
+
+    public Departamento(int idDept, String nombreDept) {
+        this.idDept = idDept;
+        this.nombreDept = nombreDept;
+    }
+
+    public int getIdDept() {
+        return idDept;
+    }
+
+    public void setIdDept(int idDept) {
+        this.idDept = idDept;
+    }
+
+    public String getNombreDept(String string) {
+        return nombreDept;
+    }
+
+    public void setNombreDept(String nombreDept) {
+        this.nombreDept = nombreDept;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" + "idDept=" + idDept + ", nombreDept=" + nombreDept + '}';
+    }
+    
+    
+    
 }
+
